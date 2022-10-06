1371beb542d024c549fa0a600448726f0e67d2ce
Alejandro Martell Guevara <alejandro.mguevara@mcsystems.com>
2022-10-06 11:37:39 -0400

    feat: adding new test

--- __test__/app.spec.ts
+++ __test__/app.spec.ts
@@ -3,3 +3,7 @@ import {sum} from "../src";
 test("testing sum function", () => {
     expect(sum(1, 2)).toBe(1 + 2);
 })
+
+test("testing multiply function", () => {
+    expect(sum(2, 4)).toBe(2 * 4);
+})
