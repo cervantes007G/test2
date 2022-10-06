90e033e07e78908ecf747c3a56f3ebaebe8ee982
Alejandro Martell Guevara <alejandro.mguevara@mcsystems.com>
2022-10-06 11:54:10 -0400

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.interface.ts',
-    '!src/**/*.types.ts',
-    '!**/node_modules/**',
+    './src/*'
   ],
 };
