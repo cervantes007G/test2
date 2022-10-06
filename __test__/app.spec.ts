7b3021a923d4d0a8624d1fc0fb753eff58625e65
Alejandro Martell Guevara <alejandro.mguevara@mcsystems.com>
2022-10-06 11:39:56 -0400

    fix: fixing new tests

--- __test__/app.spec.ts
+++ __test__/app.spec.ts
@@ -1,9 +1,9 @@
-import {sum} from "../src";
+import {multiply, sum} from "../src";
 
 test("testing sum function", () => {
     expect(sum(1, 2)).toBe(1 + 2);
 })
 
 test("testing multiply function", () => {
-    expect(sum(2, 4)).toBe(2 * 4);
+    expect(multiply(2, 4)).toBe(2 * 4);
 })
